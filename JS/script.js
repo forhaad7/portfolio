644b4145a23a7ba98d1a0dc7e843902bf9b5d2bf
forhaad7 <forhad6666@gmail.com>
2022-11-13 02:05:47 +0600

    side social bar animated

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,8 @@
 
+
+
+
+
 //Mutli typing effect start -----------------------------------------
 
 	const text = document.querySelector(".sec-text");
@@ -44,3 +48,23 @@
 		}
 
 //Up Button end
+
+
+//scrool up smooth animation
+function reveal() {
+	var reveals = document.querySelectorAll(".reveal");
+
+	for (var i = 0; i < reveals.length; i++) {
+		var windowHeight = window.innerHeight;
+		var elementTop = reveals[i].getBoundingClientRect().top;
+		var elementVisible = 150;
+
+		if (elementTop < windowHeight - elementVisible) {
+			reveals[i].classList.add("active");
+		} else {
+			reveals[i].classList.remove("active");
+		}
+	}
+}
+
+window.addEventListener("scroll", reveal);
